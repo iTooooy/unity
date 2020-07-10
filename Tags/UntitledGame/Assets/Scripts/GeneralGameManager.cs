3cd7635211242ed582ef68bc6b433268741d287e
NikeFish <591091027@qq.com>
2020-07-10 13:05:10 +0800

    项目上传

---
+++ Tags/UntitledGame/Assets/Scripts/GeneralGameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GeneralGameManager : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("TeamUnityCredits");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
---
+++ Tags/UntitledGame/Assets/Scripts/MouseRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotation : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform player;
+    float xRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        player.Rotate(Vector3.up * mouseX);
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+    }
+}
